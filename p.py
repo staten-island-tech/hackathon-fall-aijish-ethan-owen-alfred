f5069af6f82d36b7f3bbd3395b4fa3876891523c
ethanxie25 <randomized0938@gmail.com>
2024-11-22 09:06:15 -0500

    cv

--- p.py
+++ p.py
@@ -16,3 +16,17 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Music Squares')
 
 pygame.font.SysFont("Comic Sans", 48)
+
+class Tile:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.rect = pygame.Rect(self.x, self.y, tile_width, tile_height)
+
+    def move(self):
+        self.y += fall_speed
+        self.rect.y = self.y
+
+    def draw(self, is_pressed=False):
+        color = (0, 200, 0) if is_pressed else tiles
+        pygame.draw.rect(screen, color, self.rect, border_radius=10)
